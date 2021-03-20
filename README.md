98c9c9f296dcba4be651f5f94daca74d699b7dab
christopherFlynn <cmf180003@utdallas.edu>
2021-03-20 14:24:24 -0500

    Basic functionality for +,-,/,* added

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # js_calculator
+
+VERY BASIC Calculator
+
+TODO:
+* Add support for decimal input
+* Make it look nice
+* Add backspace/keyboard support
--- calculator.js
+++ calculator.js
@@ -1,14 +1,14 @@
 function add(num1, num2) {
-    return num1 + num2;
+    return parseInt(num1,10) + parseInt(num2,10);
 }
 function sub(num1, num2) {
-    return num1 - num2;
+    return parseInt(num1,10) - parseInt(num2,10);
 }
 function mult(num1, num2) {
-    return num1 * num2;
+    return parseInt(num1,10) * parseInt(num2,10);
 }
 function div(num1, num2) {
-    return num1 / num2;
+    return parseInt(num1,10) / parseInt(num2,10);
 }
 function operate(num1, num2, op) {
     switch(op) {
@@ -19,9 +19,27 @@ function operate(num1, num2, op) {
         case '*':
             return mult(num1, num2);
         case '/':
+            if(num2 == 0) {
+                return "Get bent!";
+            }
             return div(num1, num2); 
     }
 }
+function solve(equation) {
+    let numsAndOps = equation.split(" ");
+    if(numsAndOps[numsAndOps.length-1] == "") {
+        return "ERROR: Please enter a solvable equation";
+    }
+    let answer = numsAndOps[0];
+    console.log(numsAndOps);
+    for(let i = 0; i < numsAndOps.length-2; i+=2) {
+       answer = operate(answer, numsAndOps[i+2], numsAndOps[i+1]);
+    }
+    console.log(answer);
+    return answer;
+}
+const displayDiv = document.querySelector('#display');
+let dispText = "";
 const numContainer = document.querySelector("#numbers");
 for(let id = 0; id < 10; ++id) {
     const numBtn = document.createElement('button');
@@ -29,7 +47,9 @@ for(let id = 0; id < 10; ++id) {
     numBtn.textContent = id;
     numBtn.style.margin = '7px';
     numBtn.addEventListener("click", () => {
-        alert(id);
+        //alert(id);
+        dispText += `${id}`;
+        displayDiv.textContent = dispText;
     });
     numContainer.appendChild(numBtn);
 }
@@ -41,11 +61,20 @@ operators.forEach(element => {
     opBtn.textContent = element;
     opBtn.style.margin = '7px';
     opBtn.addEventListener("click", () => {
-        alert(element);
+        //alert(element);
+        dispText += " " + element + " ";
+        displayDiv.textContent = dispText;
     });
     opContainer.appendChild(opBtn);
 });
-
-
+//setup for calculator buttons
+document.querySelector('#calculate-btn').addEventListener("click", () => {
+    dispText = solve(dispText);
+    displayDiv.textContent = dispText;
+})
+document.querySelector('#clear-btn').addEventListener("click", () => {
+    dispText = "";
+    displayDiv.textContent = dispText;
+})
 
 //console.log(numContainer);
